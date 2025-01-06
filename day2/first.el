57a6370166566bd91839a4b8ac2fccf532447cee
Jakub Piskiewicz <jakub@piskiewicz.org>
2025-01-06 12:25:31 +0100

    Day 2; part 1

--- day2/first.el
+++ day2/first.el
@@ -5,8 +5,11 @@
   (buffer-string)))
 
 (let* ((reports (mapcar (lambda (str) (mapcar #'string-to-number (split-string str " "))) (butlast (split-string input "\n"))))
-      (indices (number-sequence 0 (- (length (car reports)) 1)))
-      (diffs (mapcar (lambda (report) (mapcar (lambda (i) (- (nth i report) (if (equal i 0) (nth i report) (nth (- i 1) report)))) indices)) reports)))
-      (message "%S" diffs))
+      (diffs (mapcar (lambda (report) (cdr (mapcar (lambda (i) (- (nth i report) (if (equal i 0) (nth i report) (nth (- i 1) report)))) (number-sequence 0 (- (length report) 1))))) reports))
+      (is-monotonic (mapcar (lambda (report) (= (length (delete-dups (mapcar (lambda (x) (>= x 0)) report))) 1)) diffs))
+      (contained-in-range (mapcar (lambda (report) (and (> (seq-min (mapcar #'abs report)) 0) (<= (seq-max (mapcar #'abs report)) 3))) diffs))
+      (safe (mapcar (lambda (i) (and (nth i is-monotonic) (nth i contained-in-range))) (number-sequence 0 (- (length reports) 1)))))
+      (message "%S" (length (remove nil safe))))
+
 
 ;;; first.el ends here
